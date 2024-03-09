99e361b603b5a9d9c827524ab807a39a79b335ba
Norberto Caceres <norberto.e.888@gmail.com>
2024-03-09 16:20:37 -0500

    bootstrap payment service

--- services/payment/src/main.ts
+++ services/payment/src/main.ts
@@ -1,22 +1,28 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+import { Config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const configService = app.get(ConfigService);
+  const cookieSecret = configService.get<Config['cookie']>('cookie').secret;
+
+  app.use(cookieParser(cookieSecret));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+    })
   );
+
+  const port = configService.get<Config['misc']>('misc').port;
+  await app.listen(port);
+
+  Logger.log(`🚀 Payment service is running on: http://localhost:${port}`);
 }
 
 bootstrap();
