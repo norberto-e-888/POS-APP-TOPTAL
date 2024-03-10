ea5f65a3d4c2e0b130800139b5f6bb0d1b00b8a1
Norberto Caceres <norberto.e.888@gmail.com>
2024-03-09 20:36:36 -0500

    bootstrap shipping service

--- services/shipping/src/app/app.module.ts
+++ services/shipping/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { config } from '../config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+  ],
+  providers: [],
+  controllers: [],
 })
 export class AppModule {}
