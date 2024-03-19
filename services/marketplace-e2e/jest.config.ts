b071917abbda70bb873c31b0eb1515451295bd47
Norberto Caceres <norberto.e.888@gmail.com>
2024-03-18 23:37:46 -0500

    rename order service to marketplace

--- services/marketplace-e2e/jest.config.ts
+++ services/marketplace-e2e/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'services-order-e2e',
+  displayName: 'services-marketplace-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
